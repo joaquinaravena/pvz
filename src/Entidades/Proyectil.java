fa21d4c4a76e8aedba7d954e1459d28e0f85049e
Ezequiel Aguilar <ezequielmaximiliano123@gmail.com>
2022-11-03 11:13:18 -0300

    cambio proyectiles

--- src/Entidades/Proyectil.java
+++ src/Entidades/Proyectil.java
@@ -1,5 +1,11 @@
 package Entidades;
 
-public abstract class Proyectil {
-
+public abstract class Proyectil extends Entidad{
+	protected int valorAccion;
+	public void mover() {
+		x = x+5;
+	}
+	public void chocar(Zombie z) {
+		z.restarVida(valorAccion);
+	}
 }
