d6b86d65021b49996f1ae0848c6926d0f581dea8
Joaquin <joaco.aravena02@gmail.com>
2022-11-04 15:28:27 -0300

    cambios en ventana

--- src/GUI/Ventana.java
+++ src/GUI/Ventana.java
@@ -1,22 +1,17 @@
 package GUI;
 import Logica.*;
 
+
 import java.awt.EventQueue;
-import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.Window.Type;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JToggleButton;
-import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 @SuppressWarnings("serial")
 public class Ventana extends JFrame{
@@ -58,10 +53,8 @@ public class Ventana extends JFrame{
 		panelDia.setLayout(null);
 		
 		JLabel lblFondo = new JLabel();
-		//ImageIcon fondoDia = new ImageIcon(Toolkit.getDefaultToolkit().getImage("src/Imagenes/FondoDiaNuevo2.png").getScaledInstance(734, 461, 0));
-		//lblNewLabel.setIcon(fondoDia);
-		lblFondo.setIcon(new ImageIcon(Ventana.class.getResource("/Imagenes/FondoDiaNuevo.png")));
 		lblFondo.setBounds(0, 50, 684, 411);
+		lblFondo.setIcon(new ImageIcon(new ImageIcon(Ventana.class.getResource("/Imagenes/FondoDia.png")).getImage().getScaledInstance(lblFondo.getWidth(), lblFondo.getHeight(), DO_NOTHING_ON_CLOSE)));
 		panelDia.add(lblFondo);
 		
 		JPanel panelPlantas = new JPanel();
@@ -69,33 +62,18 @@ public class Ventana extends JFrame{
 		panelPlantas.setBounds(0, 0, 684, 52);
 		panelDia.add(panelPlantas);
 		
-		JButton btnPonerMusica = new JButton("Parar Musica");
-		panelPlantas.add(btnPonerMusica);
-		btnPonerMusica.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				miJuego.reproducirMusica();
-			}
-		});
-		
-		JButton btnPararMusica = new JButton("Poner Musica");
-		panelPlantas.add(btnPararMusica);
-		btnPararMusica.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				miJuego.pararMusica();
-			}
-		});
-		
-		JLabel lblSoles = new JLabel("Soles actuales");
-		panelPlantas.add(lblSoles);
-		
-		JToggleButton botonPlanta1 = new JToggleButton();		
-		panelPlantas.add(botonPlanta1);
-		
+		JToggleButton botonPlanta1 = new JToggleButton();
+		botonPlanta1.setBounds(204, 7, 33, 9);
 		JToggleButton botonPlanta2 = new JToggleButton();
-		panelPlantas.add(botonPlanta2);
-		
+		botonPlanta2.setBounds(270, 7, 33, 9);
 		JToggleButton botonPlanta3 = new JToggleButton();
-		panelPlantas.add(botonPlanta3);
+		botonPlanta3.setBounds(336, 7, 33, 20);		
+		
+		JToggleButton botonMusica = new JToggleButton();
+		botonMusica.setBounds(460, 7, 41, 39);
+		ImageIcon imgg = new ImageIcon(Ventana.class.getResource("/Imagenes/stop.jpg"));
+		//botonMusica.setIcon(new ImageIcon(imgg.getImage().getScaledInstance(20, 20, DO_NOTHING_ON_CLOSE)));
+		botonMusica.setSelected(true);
 		
 		botonPlanta1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -104,18 +82,41 @@ public class Ventana extends JFrame{
 			}
 		});
 		
+		
 		botonPlanta2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				botonPlanta1.setSelected(false);
 				botonPlanta3.setSelected(false);
 			}
 		});
+		
+		
 		botonPlanta3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				botonPlanta2.setSelected(false);
 				botonPlanta1.setSelected(false);
 			}
 		});
+		
+		
+		botonMusica.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(botonMusica.isSelected()) 
+					miJuego.reproducirMusica();
+				else 
+					miJuego.pararMusica();
+			}
+		});
+		
+		
+		JLabel lblSoles = new JLabel("Soles actuales");
+		lblSoles.setBounds(22, 32, 68, 14);
+		panelPlantas.setLayout(null);
+		panelPlantas.add(botonPlanta1);
+		panelPlantas.add(botonPlanta2);
+		panelPlantas.add(botonPlanta3);
+		panelPlantas.add(botonMusica);
+		panelPlantas.add(lblSoles);
 		
 	}
 }
