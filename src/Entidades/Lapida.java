9cc1b244b29863108314540d1d8220ad99675c0a
Julian Gallardo <juliangallardo26603@gmail.com>
2022-11-08 22:49:10 -0300

    Soles y lapidas agregadas

---
+++ src/Entidades/Lapida.java
@@ -0,0 +1,17 @@
+package Entidades;
+
+public class Lapida {
+	
+	public Lapida() {
+		super();
+	}
+	
+	public void aparecerZombie() {
+		//Preguntar chicos
+	}
+	
+	public Lapida clone() {
+		Lapida aRetornar=new Lapida();
+		return aRetornar;
+	}
+}
--- src/Entidades/Sol.java
+++ src/Entidades/Sol.java
@@ -2,4 +2,16 @@ package Entidades;
 
 public class Sol extends Entidad{
 	
+	public Sol() {
+		super();
+	}
+	
+	public void mover() {
+		y=y-10;
+	}
+	
+	public Sol clone() {
+		Sol aRetornar=new Sol();
+		return aRetornar;
+	}
 }
